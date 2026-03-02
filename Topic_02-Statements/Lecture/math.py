ee9b2c3df4b4625bb2c8def74d98db81640b0466
Monika Fras <G00472984@atu.ie>
2026-03-02 10:10:43 +0000

    Topic_02-Statements

---
+++ Topic_02-Statements/Lecture/math.py
@@ -0,0 +1,8 @@
+# math
+# messing around with math
+# Author Monika Fras
+
+floatnum = 0.29
+
+answer = floatnum
+print(answer)
---
+++ Topic_02-Statements/Lecture/saymyname.py
@@ -0,0 +1,12 @@
+# saymyname.py
+# messing with inputs
+# Author Monika Fras
+
+'''
+name = input("say your name:")
+print ("Hello "+ name)
+'''
+
+number = int (input("enter a number: "))
+answer = number * 4
+print(answer)
